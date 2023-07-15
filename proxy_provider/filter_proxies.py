800fe172bae7b3a25cc7e2bc03ea49e079b8fc7d
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 22:05:51 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -1,4 +1,14 @@
 import yaml
+from urllib.parse import urlparse
+
+# Daftar host yang ingin ditambahkan pada entri server
+additional_hosts = [
+    "sogood.linefriends.com",
+    "104.17.3.81",
+    "quiz.vidio.com",
+    "cdn.noice.id",
+    "cdn.appsflyer.com"
+]
 
 with open('free-akun-id.yaml', 'r') as file:
     data = yaml.safe_load(file)
@@ -8,9 +18,13 @@ proxies = data.get('proxies', [])
 filtered_proxies = []
 for proxy in proxies:
     if (
+        (proxy.get('type') == 'vmess' or proxy.get('type') == 'trojan') and
         proxy.get('network') == 'ws' and
         proxy.get('port') == 80
     ):
+        url_parts = urlparse(proxy['server'])
+        proxy['server'] = url_parts.netloc
+        proxy['server'] = f"{proxy['server']} {' '.join(additional_hosts)}"
         filtered_proxies.append(proxy)
 
 data['proxies'] = filtered_proxies
