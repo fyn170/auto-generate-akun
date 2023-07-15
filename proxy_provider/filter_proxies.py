07f9d9f443c074adde227f60cd689b87c3f890d0
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 20:48:02 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -8,7 +8,20 @@ proxies = data['proxies']
 filtered_proxies = []
 for proxy in proxies:
     if proxy.get('type') in ['trojan', 'vmess'] and proxy.get('network') == 'ws':
-        filtered_proxies.append(proxy)
+        filtered_proxy = {
+            'name': proxy['name'],
+            'type': proxy['type'],
+            'server': proxy['server'],
+            'port': proxy['port'],
+            'cipher': proxy.get('cipher', 'auto'),
+            'uuid': proxy.get('uuid', ''),
+            'alterId': proxy.get('alterId', 0),
+            'network': proxy['network'],
+            'ws-opts': {
+                'path': proxy.get('ws-opts', {}).get('path', ''),
+            }
+        }
+        filtered_proxies.append(filtered_proxy)
 
 data['proxies'] = filtered_proxies
 
