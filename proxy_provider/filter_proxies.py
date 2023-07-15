eb731bb05ae6a5ca51f9f739d867e47dd925fb96
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 21:53:46 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -7,8 +7,29 @@ proxies = data.get('proxies', [])
 
 filtered_proxies = []
 for proxy in proxies:
-    proxy.pop('server', None)  # Menghapus bagian IP dari entri proxies
-    filtered_proxies.append(proxy)
+    if (
+        (proxy.get('type') == 'vmess' and proxy.get('network') == 'ws') or
+        (proxy.get('type') == 'trojan' and proxy.get('network') == 'ws')
+    ):
+        filtered_proxy = {
+            'name': proxy.get('name', ''),
+            'server': 'null',
+            'port': proxy.get('port', ''),
+            'type': proxy.get('type', ''),
+            'uuid': proxy.get('uuid', ''),
+            'alterId': proxy.get('alterId', 0),
+            'cipher': proxy.get('cipher', 'auto'),
+            'tls': proxy.get('tls', False),
+            'skip-cert-verify': proxy.get('skip-cert-verify', False),
+            'servername': proxy.get('servername', ''),
+            'network': proxy.get('network', ''),
+            'ws-opts': {
+                'path': proxy['ws-opts']['path'],
+                'headers': proxy['ws-opts'].get('headers', {})
+            },
+            'udp': proxy.get('udp', False)
+        }
+        filtered_proxies.append(filtered_proxy)
 
 data['proxies'] = filtered_proxies
 
