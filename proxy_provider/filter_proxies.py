e8161166d53d4b46dc490ffa35523cb70cc74bef
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 21:47:10 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -1,10 +1,4 @@
-import random
-import string
 import yaml
-
-def generate_random_name(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
 
 with open('free-akun-id.yaml', 'r') as file:
     data = yaml.safe_load(file)
@@ -13,10 +7,11 @@ proxies = data.get('proxies', [])
 
 filtered_proxies = []
 for proxy in proxies:
-    random_name = generate_random_name(8)
-    name = f"{proxy.get('name', '')}-{random_name}🇮🇩"
-    proxy['name'] = name
-    filtered_proxies.append(proxy)
+    if (
+        proxy.get('network') == 'ws' and
+        proxy.get('type') in ['vmess', 'trojan']
+    ):
+        filtered_proxies.append(proxy)
 
 data['proxies'] = filtered_proxies
 
