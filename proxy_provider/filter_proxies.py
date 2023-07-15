3d6ca5313e441915632725fac223b02fb65e122b
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 22:01:19 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -1,5 +1,4 @@
 import yaml
-from urllib.parse import urlparse
 
 with open('free-akun-id.yaml', 'r') as file:
     data = yaml.safe_load(file)
@@ -9,11 +8,9 @@ proxies = data.get('proxies', [])
 filtered_proxies = []
 for proxy in proxies:
     if (
-        (proxy.get('type') == 'vmess' and proxy.get('network') == 'ws') or
-        (proxy.get('type') == 'trojan' and proxy.get('network') == 'ws')
+        proxy.get('network') == 'ws' and
+        proxy.get('port') == 80
     ):
-        url_parts = urlparse(proxy['server'])
-        proxy['server'] = url_parts.netloc
         filtered_proxies.append(proxy)
 
 data['proxies'] = filtered_proxies
