367179102ba83fab16b2061df477b0a8e12d2c08
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 21:00:16 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -1,34 +1,32 @@
-import yaml
+import ruamel.yaml
 
 with open('free-akun-id.yaml', 'r') as file:
-    data = yaml.safe_load(file)
+    data = ruamel.yaml.round_trip_load(file)
 
 proxies = data['proxies']
 
 filtered_proxies = []
 for proxy in proxies:
     if proxy.get('type') == 'vmess' and proxy.get('network') == 'ws':
-        filtered_proxy = {
-            'name': proxy['name'],
-            'server': proxy['server'],
-            'port': proxy['port'],
-            'type': proxy['type'],
-            'uuid': proxy['uuid'],
-            'alterId': proxy.get('alterId', 0),
-            'cipher': proxy.get('cipher', 'auto'),
-            'tls': proxy.get('tls', False),
-            'skip-cert-verify': proxy.get('skip-cert-verify', False),
-            'servername': proxy.get('servername', ''),
-            'network': proxy['network'],
-            'ws-opts': {
-                'path': proxy['ws-opts']['path'],
-                'headers': proxy['ws-opts'].get('headers', {})
-            },
-            'udp': proxy.get('udp', False)
-        }
+        filtered_proxy = ruamel.yaml.comments.CommentedMap()
+        filtered_proxy['name'] = proxy['name']
+        filtered_proxy['server'] = proxy['server']
+        filtered_proxy['port'] = proxy['port']
+        filtered_proxy['type'] = proxy['type']
+        filtered_proxy['uuid'] = proxy['uuid']
+        filtered_proxy['alterId'] = proxy.get('alterId', 0)
+        filtered_proxy['cipher'] = proxy.get('cipher', 'auto')
+        filtered_proxy['tls'] = proxy.get('tls', False)
+        filtered_proxy['skip-cert-verify'] = proxy.get('skip-cert-verify', False)
+        filtered_proxy['servername'] = proxy.get('servername', '')
+        filtered_proxy['network'] = proxy['network']
+        filtered_proxy['ws-opts'] = ruamel.yaml.comments.CommentedMap()
+        filtered_proxy['ws-opts']['path'] = proxy['ws-opts']['path']
+        filtered_proxy['ws-opts']['headers'] = proxy['ws-opts'].get('headers', ruamel.yaml.comments.CommentedMap())
+        filtered_proxy['udp'] = proxy.get('udp', False)
         filtered_proxies.append(filtered_proxy)
 
 data['proxies'] = filtered_proxies
 
 with open('free-akun-id.yaml', 'w') as file:
-    yaml.dump(data, file)
+    ruamel.yaml.round_trip_dump(data, file)
