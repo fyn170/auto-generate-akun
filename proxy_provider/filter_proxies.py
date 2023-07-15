b9ab33526a4ddb5b10d027a5496c30fd4de24770
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 21:56:29 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -11,25 +11,8 @@ for proxy in proxies:
         (proxy.get('type') == 'vmess' and proxy.get('network') == 'ws') or
         (proxy.get('type') == 'trojan' and proxy.get('network') == 'ws')
     ):
-        filtered_proxy = {
-            'name': proxy.get('name', ''),
-            'server': 'null',
-            'port': proxy.get('port', ''),
-            'type': proxy.get('type', ''),
-            'uuid': proxy.get('uuid', ''),
-            'alterId': proxy.get('alterId', 0),
-            'cipher': proxy.get('cipher', 'auto'),
-            'tls': proxy.get('tls', False),
-            'skip-cert-verify': proxy.get('skip-cert-verify', False),
-            'servername': proxy.get('servername', ''),
-            'network': proxy.get('network', ''),
-            'ws-opts': {
-                'path': proxy['ws-opts']['path'],
-                'headers': proxy['ws-opts'].get('headers', {})
-            },
-            'udp': proxy.get('udp', False)
-        }
-        filtered_proxies.append(filtered_proxy)
+        proxy['server'] = proxy['server'].split('//')[1]  # Memisahkan host dari URL
+        filtered_proxies.append(proxy)
 
 data['proxies'] = filtered_proxies
 
