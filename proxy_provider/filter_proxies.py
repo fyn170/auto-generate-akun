6919a8268c0b5dd1452050f7d44b8c8fb8024244
Fynn <82778517+fyn170@users.noreply.github.com>
2023-07-15 21:09:23 +0700

    update filter_proxies.py

--- proxy_provider/filter_proxies.py
+++ proxy_provider/filter_proxies.py
@@ -1,32 +1,16 @@
-import ruamel.yaml
+import yaml
 
 with open('free-akun-id.yaml', 'r') as file:
-    data = ruamel.yaml.round_trip_load(file)
+    data = yaml.safe_load(file)
 
 proxies = data['proxies']
 
 filtered_proxies = []
 for proxy in proxies:
-    if proxy.get('type') == 'vmess' and proxy.get('network') == 'ws':
-        filtered_proxy = ruamel.yaml.comments.CommentedMap()
-        filtered_proxy['name'] = proxy['name']
-        filtered_proxy['server'] = proxy['server']
-        filtered_proxy['port'] = proxy['port']
-        filtered_proxy['type'] = proxy['type']
-        filtered_proxy['uuid'] = proxy['uuid']
-        filtered_proxy['alterId'] = proxy.get('alterId', 0)
-        filtered_proxy['cipher'] = proxy.get('cipher', 'auto')
-        filtered_proxy['tls'] = proxy.get('tls', False)
-        filtered_proxy['skip-cert-verify'] = proxy.get('skip-cert-verify', False)
-        filtered_proxy['servername'] = proxy.get('servername', '')
-        filtered_proxy['network'] = proxy['network']
-        filtered_proxy['ws-opts'] = ruamel.yaml.comments.CommentedMap()
-        filtered_proxy['ws-opts']['path'] = proxy['ws-opts']['path']
-        filtered_proxy['ws-opts']['headers'] = proxy['ws-opts'].get('headers', ruamel.yaml.comments.CommentedMap())
-        filtered_proxy['udp'] = proxy.get('udp', False)
-        filtered_proxies.append(filtered_proxy)
+    if proxy.get('type') in ['vmess', 'trojan'] and proxy.get('network') == 'ws' and proxy.get('country') == '🇮🇩':
+        filtered_proxies.append(proxy)
 
 data['proxies'] = filtered_proxies
 
 with open('free-akun-id.yaml', 'w') as file:
-    ruamel.yaml.round_trip_dump(data, file)
+    yaml.dump(data, file)
